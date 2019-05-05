ec66cf59b1addd0d09c4fe23a8a60f2654aa4df2
Nikita Kogut <glinka1202@gmail.com>
2019-05-05 23:20:34 +0300

    Added logarithm questions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,7 +65,7 @@ export default {
       this.questionsPointer = 0;
     },
     buildQuestionsList() {
-      this.selectedCategories = ['combinatorics'];
+      this.selectedCategories = ['logarithms'];
       Tests.tests.forEach((q) => {
         if (this.selectedCategories.includes(q.category)) this.questions.push(q);
       });
