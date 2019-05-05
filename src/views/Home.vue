d47d94af49f8f307dd3f7ece72bfcc7478df23bb
Nikita Kogut <glinka1202@gmail.com>
2019-05-05 23:08:11 +0300

    Added combinatorics questions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,7 +65,7 @@ export default {
       this.questionsPointer = 0;
     },
     buildQuestionsList() {
-      //this.selectedCategories = ['integral'];
+      this.selectedCategories = ['combinatorics'];
       Tests.tests.forEach((q) => {
         if (this.selectedCategories.includes(q.category)) this.questions.push(q);
       });
