fb67fb5170b71464c41b5cc07085ca32fdc86ca4
Nikita Kogut <glinka1202@gmail.com>
2019-05-06 18:21:10 +0300

    Added rectangle formulas

--- src/views/Formulas.vue
+++ src/views/Formulas.vue
@@ -65,7 +65,7 @@ export default {
       this.questionsPointer = 0;
     },
     buildQuestionsList() {
-      this.selectedCategories = ['triangle'];
+      this.selectedCategories = ['rectangle'];
       Tests.tests.forEach((q) => {
         if (this.selectedCategories.includes(q.category)) this.questions.push(q);
       });
